cc76d15fe4b62b89644f43fbfa86c5514a081550
Drew Taylor <drew@chainofinsight.com>
2022-10-01 17:33:50 -0400

    Semantic - code style

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ pub type Extension = Option<Metadata>;
 pub type Cw721MetadataContract<'a> = cw721_base::Cw721Contract<'a, Extension, Empty, Empty, Empty>;
 
 pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension, Empty>;
-pub type UpdateMetadataMsg=cw721_base::msg::UpdateMetadataMsg<Extension>;
+pub type UpdateMetadataMsg = cw721_base::msg::UpdateMetadataMsg<Extension>;
 
 const CONTRACT_NAME: &str = "crates.io:archid";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
