627b49264e44acbd0297e0ecd4a95a601c424e23
Drew Taylor <drew@chainofinsight.com>
2022-10-11 11:54:34 -0400

    patch - renames package 'archid' to 'archid-token' and implements package name change from the other repo ('cw721_base' renamed to 'cw721-archid')

--- examples/schema.rs
+++ examples/schema.rs
@@ -8,7 +8,7 @@ use cw721::{
     AllNftInfoResponse, ApprovalResponse, ApprovalsResponse, ContractInfoResponse, NftInfoResponse,
     NumTokensResponse, OperatorsResponse, OwnerOfResponse, TokensResponse,
 };
-use archid::{ExecuteMsg, Extension, InstantiateMsg, MinterResponse, QueryMsg};
+use archid_token::{ExecuteMsg, Extension, InstantiateMsg, MinterResponse, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
